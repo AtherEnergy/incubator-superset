3c714586b5437a33e89c76dd536e99ad55ccb5c6
chaitanya-ather <chaitanya.h@atherenergy.com>
2017-12-08 10:40:16 +0530

    added startup script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
+git pull origin master
 cd ./superset/assets
 yarn
 yarn build
 cd ../..
-python setup.py install
+sudo python setup.py install
 superset db upgrade
 superset init
 outdir=/var/log/superset/
 echo $outdir
 sudo mkdir -p $outdir
-source psyduckenv/bin/activate
 sudo superset runserver --port 8001 > $outdir/out.log 2> $outdir/error.log & echo $! > $outdir/pid
 
